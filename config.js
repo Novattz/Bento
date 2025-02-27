c58669f56f3b1d8985f2146a0868b5b2bfca8ac7
Tickbase <47538996+Novattz@users.noreply.github.com>
2025-02-27 17:02:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: '6453319', // Write here your API Key
+	weatherKey: 'aed245af7d100e6856187da42d17aa9a', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
